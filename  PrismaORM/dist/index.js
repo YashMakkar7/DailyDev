906a0cabeba3db28c17b970c983745a937174bcd
Yash Makkar <yashmakkar777@gmail.com>
2025-01-26 19:22:54 +0530

    expressyfy the app

---  PrismaORM/dist/index.js
+++  PrismaORM/dist/index.js
@@ -8,10 +8,38 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
-// similar to import {mongoose} from mongoose 
-const client = new client_1.PrismaClient(); // generates instance of prisma client class
+const express_1 = __importDefault(require("express"));
+// similar to import {mongoose} from mongoose
+const client = new client_1.PrismaClient({ log: ["query"] }); // generates instance of prisma client class
+const app = (0, express_1.default)();
+//@ts-ignore
+client.$on("query", (e) => {
+    //@ts-ignore
+    console.log("Query:", e.query); // Logs the raw SQL query
+});
+app.get("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield client.users.findMany();
+    res.json({
+        users
+    });
+}));
+app.get("/todos/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    const user = yield client.users.findFirst({
+        where: {
+            id: Number(id)
+        },
+        select: {
+            todos: true
+        }
+    });
+    res.json({ user });
+}));
 function createUser() {
     return __awaiter(this, void 0, void 0, function* () {
         yield client.users.create({
@@ -19,8 +47,8 @@ function createUser() {
                 username: "yash",
                 password: "yash123",
                 age: 20,
-                city: "Faridabad"
-            }
+                city: "Faridabad",
+            },
         });
     });
 }
@@ -31,18 +59,32 @@ function findAndInfo() {
                 id: 1,
             },
             select: {
-                username: true
-            }
+                // only give username
+                username: true,
+            },
         });
         console.log(user === null || user === void 0 ? void 0 : user.username);
+    });
+}
+function findAndTodos() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const user = yield client.users.findFirst({
+            where: {
+                id: 1,
+            },
+            include: {
+                todos: true,
+            },
+        });
+        console.log(user);
     });
 }
 function deleteUser() {
     return __awaiter(this, void 0, void 0, function* () {
         yield client.users.delete({
             where: {
-                id: 1
-            }
+                id: 1,
+            },
         });
     });
 }
@@ -50,22 +92,25 @@ function updateUser() {
     return __awaiter(this, void 0, void 0, function* () {
         yield client.users.update({
             where: {
-                id: 1
+                id: 1,
             },
             data: {
-                username: "yashMakkar"
-            }
+                username: "yashMakkar",
+            },
         });
     });
 }
 function findUser() {
     return __awaiter(this, void 0, void 0, function* () {
         yield client.users.findFirst({
+            // find for all entry and findFirst for one entry
             where: {
-                id: 1
-            }
+                id: 1,
+            },
         });
     });
 }
-createUser();
-findAndInfo();
+findAndTodos();
+app.listen(3000, () => {
+    console.log("Server is running");
+});
---  PrismaORM/src/index.ts
+++  PrismaORM/src/index.ts
@@ -1,55 +1,96 @@
 import { PrismaClient } from "@prisma/client";
+import express from "express";
+// similar to import {mongoose} from mongoose
+const client = new PrismaClient({ log: ["query"] }); // generates instance of prisma client class
+const app = express();
 
-// similar to import {mongoose} from mongoose 
+//@ts-ignore
+client.$on("query", (e) => {
+    //@ts-ignore
+  console.log("Query:", e.query); // Logs the raw SQL query
+});
 
-const client = new PrismaClient(); // generates instance of prisma client class
-
-async function createUser() {
-    await client.users.create({
-        data:{
-            username:"yash",
-            password:"yash123",
-            age:20,
-            city:"Faridabad"
-        }
+app.get("/users",async(req,res)=>{
+    const users = await client.users.findMany();
+    res.json({
+        users
     })
-}
-async function findAndInfo() {
+})
+app.get("/todos/:id",async(req,res)=>{
+    const id = req.params.id as unknown as string;
     const user = await client.users.findFirst({
         where:{
-            id:1,
+            id:Number(id)
         },
-        select:{ // only give username
-            username:true
+        select:{
+            todos:true
         }
     })
-    console.log(user?.username)
-}
+    res.json({user})
+})
 
+async function createUser() {
+  await client.users.create({
+    data: {
+      username: "yash",
+      password: "yash123",
+      age: 20,
+      city: "Faridabad",
+    },
+  });
+}
+async function findAndInfo() {
+  const user = await client.users.findFirst({
+    where: {
+      id: 1,
+    },
+    select: {
+      // only give username
+      username: true,
+    },
+  });
+  console.log(user?.username);
+}
+async function findAndTodos() {
+  const user = await client.users.findFirst({
+    where: {
+      id: 1,
+    },
+    include: {
+      todos: true,
+    },
+  });
+  console.log(user);
+}
 async function deleteUser() {
-    await client.users.delete({
-        where:{
-            id:1
-        }
-    })
+  await client.users.delete({
+    where: {
+      id: 1,
+    },
+  });
 }
 async function updateUser() {
-    await client.users.update({
-        where:{
-            id:1
-        },
-        data:{
-            username:"yashMakkar"
-        }
-    })
+  await client.users.update({
+    where: {
+      id: 1,
+    },
+    data: {
+      username: "yashMakkar",
+    },
+  });
 }
 async function findUser() {
-    await client.users.findFirst({ // find for all entry and findFirst for one entry 
-        where:{
-            id:1
-        }
-    })
+  await client.users.findFirst({
+    // find for all entry and findFirst for one entry
+    where: {
+      id: 1,
+    },
+  });
 }
 
-createUser();
-findAndInfo();
+
+findAndTodos();
+
+app.listen(3000,()=>{
+    console.log("Server is running");
+})
